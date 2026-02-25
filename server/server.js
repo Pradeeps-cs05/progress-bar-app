873494d41c529f5602815b1919c091264de2d284
Pradeeps <pradeepsivasubramanian@gmail.com>
2026-02-25 10:10:32 +0530

    Final express routing fix

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -8,16 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
+// API Routes
 app.use("/api/task", require("./routes/taskRoutes"));
 
 const PORT = process.env.PORT || 7000;
-const path = require("path");
 
-// Serve frontend build
+// Serve React build
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get((req, res) => {
